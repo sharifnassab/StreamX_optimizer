0506fd03cefb371553e4a3872f1731c2c1c2f311
Arsalan Sharifnassab <a.sharifnassab@gmail.com>
2025-10-20 11:43:46 -0600

    test1

--- stream_ac_continuous_OptDesign_prediction.py
+++ stream_ac_continuous_OptDesign_prediction.py
@@ -18,7 +18,7 @@ from sparse_init import sparse_init
 
 # NEW: our tiny logger (wandb or tensorboard)
 from logger import get_logger
-print('All imports done.')
+print('All imports done. 1')
 
 def initialize_weights(m):
     if isinstance(m, nn.Linear):
@@ -331,7 +331,7 @@ if __name__ == '__main__':
     parser.add_argument('--render', action='store_true')
 
     # NEW: logging choices/params
-    parser.add_argument('--log_backend', type=str, default='wandb_offline', choices=['tensorboard', 'wandb', 'wandb_offline', 'none'])
+    parser.add_argument('--log_backend', type=str, default='wandb', choices=['tensorboard', 'wandb', 'wandb_offline', 'none'])
     parser.add_argument('--log_dir', type=str, default='"/home/asharif/StreamX_optimizer/StreamX_optimizer/WandB_offline"', help='WandB offline log dir (if backend=wandb_offline)')  # default='runs', help='TensorBoard log dir (if backend=tensorboard)')
     parser.add_argument('--project', type=str, default='test_stream_CC', help='WandB project (if backend=wandb)')
     parser.add_argument('--run_name', type=str, default='', help='Run name for logger') # __sqrt_coeff
