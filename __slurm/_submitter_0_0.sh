9da3fdab8dba6dd1de34ebd5d7a40b239fbd5835
Arsalan Sharifnassab <a.sharifnassab@gmail.com>
2025-10-20 15:41:08 -0600

    test1

--- __slurm/_submitter_0_0.sh
+++ __slurm/_submitter_0_0.sh
@@ -2,9 +2,9 @@
 #SBATCH --account=def-sutton
 #SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=1024M
+#SBATCH --mem=2048M
 #SBATCH --array=1-1
-#SBATCH --output=/home/asharif/StreamX_optimizer/results/outputs/2025_10_20__15_40_57__test0/0_0/output_%j.txt
+#SBATCH --output=/home/asharif/StreamX_optimizer/results/outputs/2025_10_20__15_41_06__test0/0_0/output_%j.txt
 #SBATCH --constraint=granite
 
 module purge
@@ -16,7 +16,7 @@ source ~/venv_streamx/bin/activate
 export PYTHONNOUSERSITE=1
 
 
-$(sed -n "${SLURM_ARRAY_TASK_ID}p" < /home/asharif/StreamX_optimizer/results/outputs/2025_10_20__15_40_57__test0/export_0_0.dat)
+$(sed -n "${SLURM_ARRAY_TASK_ID}p" < /home/asharif/StreamX_optimizer/results/outputs/2025_10_20__15_41_06__test0/export_0_0.dat)
 echo "Task ${SLURM_ARRAY_TASK_ID} started on $(hostname) at $(date)"
 python3 stream_ac_continuous_OptDesign_prediction.py --env_name=$env_name --total_steps=$total_steps --seed=$seed --gamma=$gamma --lamda=$lamda --kappa_policy=$kappa_policy --kappa_value=$kappa_value --observer_type=$observer_type --u_trace_value=$u_trace_value --entryise_normalization_value=$entryise_normalization_value --beta2_value=$beta2_value --log_backend=$log_backend --log_dir=$log_dir --project=$project --max_time=$max_time --uID=$uID
 echo "Program test finished with exit code $? at: `date`"
