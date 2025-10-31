7a847a719e52d997fad5bc412ab1b5abe272e642
Arsalan Sharifnassab <a.sharifnassab@gmail.com>
2025-10-31 15:28:37 -0600

    test1

--- __slurm/_submitter_2_0.sh
+++ __slurm/_submitter_2_0.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account=def-sutton
-#SBATCH --time=02:59:00
+#SBATCH --time=04:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=2048M
-#SBATCH --array=1-300
-#SBATCH --output=/home/asharif/scratch/StreamX_optimizer/outputs/2025_10_31__15_27_48__test0/2_0/output_%j.txt
+#SBATCH --array=1-150
+#SBATCH --output=/home/asharif/scratch/StreamX_optimizer/outputs/2025_10_31__15_28_09__test0/2_0/output_%j.txt
 #SBATCH --constraint=granite
 
 module purge
@@ -16,7 +16,7 @@ source ~/venv_streamx/bin/activate
 export PYTHONNOUSERSITE=1
 
 
-$(sed -n "${SLURM_ARRAY_TASK_ID}p" < /home/asharif/scratch/StreamX_optimizer/outputs/2025_10_31__15_27_48__test0/export_2_0.dat)
+$(sed -n "${SLURM_ARRAY_TASK_ID}p" < /home/asharif/scratch/StreamX_optimizer/outputs/2025_10_31__15_28_09__test0/export_2_0.dat)
 echo "Task ${SLURM_ARRAY_TASK_ID} started on $(hostname) at $(date)"
-python3 stream_ac_continuous.py --total_steps=$total_steps --policy_optimizer=$policy_optimizer --policy_gamma=$policy_gamma --policy_lamda=$policy_lamda --policy_lr=$policy_lr --policy_entropy_coeff=$policy_entropy_coeff --critic_gamma=$critic_gamma --critic_lamda=$critic_lamda --critic_lr=$critic_lr --observer_optimizer=$observer_optimizer --log_backend=$log_backend --log_dir=$log_dir --logging_level=$logging_level --project=$project --env_name=$env_name --critic_optimizer=$critic_optimizer --critic_kappa=$critic_kappa --critic_entrywise_normalization=$critic_entrywise_normalization --critic_beta2=$critic_beta2 --critic_sig_power=$critic_sig_power --critic_in_trace_sample_scaling=$critic_in_trace_sample_scaling --seed=$seed --max_time=$max_time --uID=$uID
+python3 stream_ac_continuous.py --total_steps=$total_steps --policy_optimizer=$policy_optimizer --policy_kappa=$policy_kappa --policy_gamma=$policy_gamma --policy_lamda=$policy_lamda --policy_lr=$policy_lr --policy_entropy_coeff=$policy_entropy_coeff --critic_optimizer=$critic_optimizer --critic_kappa=$critic_kappa --critic_gamma=$critic_gamma --critic_lamda=$critic_lamda --critic_lr=$critic_lr --log_backend=$log_backend --log_dir=$log_dir --logging_level=$logging_level --project=$project --env_name=$env_name --observer_optimizer=$observer_optimizer --observer_lamda=$observer_lamda --observer_kappa=$observer_kappa --observer_entrywise_normalization=$observer_entrywise_normalization --observer_beta2=$observer_beta2 --observer_sig_power=$observer_sig_power --observer_in_trace_sample_scaling=$observer_in_trace_sample_scaling --seed=$seed --max_time=$max_time --uID=$uID
 echo "Program test finished with exit code $? at: `date`"
